5c90f816f8af870b24f04631877158ae8af67998
Andrei Gobres <andreigobres3@gmail.com>
2026-01-03 21:00:09 +0800

    added routes

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,10 @@
+import styles from './ErrorPage.module.css'
+
+function ErrorPage() {
+
+    return(
+        <div className={styles.errorMessage}>Error 404</div>
+    );
+}
+
+export default ErrorPage
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './global-styles/variables.css'
 import './global-styles/global.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import ErrorPage from './Pages/ErrorPage/ErrorPage.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/FMC-Typing-speed-test/',
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: '*',
+    element: <ErrorPage />
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
